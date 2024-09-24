d9f186174eb7fd4bb517506945c5e0462d3d784f
Mueen Arif <muhammadmueenarif786@gmail.com>
2024-09-24 21:17:31 +0500

    get unicode character value with html css and js

---
+++ script.js
@@ -0,0 +1,27 @@
+        // code to get input from user and display unicode of the character 
+
+
+        function detectChar() {
+            var input = document.getElementById("inputType").value;
+            if (input) {
+                const unicodeValue = input.charCodeAt(0);
+                const result = `The unicode of "${input}" is ${unicodeValue}`;
+                document.getElementById("result").textContent = result;
+                //document.getElementById("inputType").value=""; 
+                // it will clear input after each result.
+        
+            } else {
+                document.getElementById("result").textContent = 'Please Enter a Character';
+            }
+        
+        }
+        
+        
+        
+        
+        // another way we can use.
+        // function Detector() {
+        //     var userText = document.getElementById("inputText").value;
+        //     var unicode = userText.charCodeAt();  
+        //     document.getElementById("result").innerHTML = "unicode of the character is " + unicode;
+        // }
